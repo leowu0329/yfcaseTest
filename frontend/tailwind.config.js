b5cdf047806c0314cf29d796db49defac1c5e0a2
leowu0329 <ryowuandjanet@gmail.com>
2025-08-04 23:23:55 +0800

    加入字型中文：Noto Sans Traditional Chinese,英文：Roboto

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'noto': ['Noto Sans TC', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
